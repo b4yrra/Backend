31b94d75440600ae9e3982ca505a27011202f9bf
25lp5214 User <25lp5214@Administrators-iMac.local>
2026-03-03 12:43:01 +0800

    Backend Learn

---
+++ server.ts
@@ -0,0 +1,75 @@
+import express, { Request, Response } from "express";
+import { title } from "node:process";
+
+let books = [
+  {
+    id: 1,
+    title: "Harry Potter 1",
+    author: "Bat",
+  },
+
+  {
+    id: 2,
+    title: "Harry Potter 2",
+    author: "Bat",
+  },
+];
+
+const server = express();
+const PORT = 8080;
+
+server.use(express.json());
+
+server.get("/", (req: Request, res: Response) => {
+  console.log("Hello");
+});
+
+server.get("/books", (req: Request, res: Response) => {
+  res.status(200).send(books);
+});
+
+server.get("/books/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const book = books.find((book) => String(book.id) === String(id));
+
+  res.status(200).send(book);
+});
+
+server.post("/books", (req: Request, res: Response) => {
+  const { title, author } = req.body;
+  const newBookId = books.length + 1;
+
+  const newBook = { id: newBookId, title, author };
+
+  books.push(newBook);
+  res.send(books);
+});
+
+server.put("/books/:id", (req: Request, res: Response) => {
+  const { title, author } = req.body;
+  const { id } = req.params;
+  const bookAuthor = "Bat";
+
+  books = books.map((book) => {
+    if (book.author === bookAuthor) {
+      const newBooks = {
+        id: book.id,
+        title: book.title,
+        author: "J.K Rowling",
+      };
+
+      return newBooks;
+    } else {
+      return book;
+    }
+  });
+
+  const book = books.find((book) => String(book.id) === String(id));
+
+  res.send(book);
+});
+
+server.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
